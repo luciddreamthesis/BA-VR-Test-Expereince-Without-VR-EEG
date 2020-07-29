1d31642b3b18b5bf3259240482f4a85bc86a2aa6
luciddreamthesis <luciddreamthesis@gmail.com>
2020-07-29 17:39:47 +0200

    Copy of VR Experince wihout Player

---
+++ 200729_Test_Experience_with_VR/Assets/LevelChanger2nd.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger2nd : MonoBehaviour
+{
+    public Animator animator;
+
+    private int levelToLoad;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Invoke("goToThirdScene", 50);
+    }
+
+    public void goToThirdScene()
+    {
+        FadeToLevel(2);
+    }
+
+    public void FadeToLevel(int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
